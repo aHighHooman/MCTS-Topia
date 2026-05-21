e9877d64cfc8c21c3c1edb97a959f8359b19ca55
aHighHooman <aHoomanOG@gmail.com>
2026-05-21 05:42:41 -0700

    try: enable tf32 for cuda nn mcts inference

--- py/search/native/mcts.py
+++ py/search/native/mcts.py
@@ -15,6 +15,11 @@ from nn.encoding import EncodedObservation, encode_observation, normalize_messag
 from nn.model import HybridPolicyValueNet
 from .cpp_extension import load_native_mcts_extension
 
+if torch.cuda.is_available():
+    torch.backends.cuda.matmul.allow_tf32 = True
+    torch.backends.cudnn.allow_tf32 = True
+    torch.set_float32_matmul_precision("high")
+
 
 class NativeSearchUnavailable(RuntimeError):
     pass
